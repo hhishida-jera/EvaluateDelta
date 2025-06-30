f99e636dbcc9a193c7f5c68ca7334b8966d5ee3f
hhishida-jera <hiroyuki.hishida@jera.co.jp>
2025-06-30 11:04:12 +0900

    Functionalization

---
+++ preprocessCSV.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+print("データ読み込み")
+
+#必要部分だけ残す
+df=pd.read_csv(r"CSV\Load_fromSnowflake.csv")
+
+# StartDatetime列をdatetime型に変換（ここが重要！）
+df["DELIVERYDATE"] = pd.to_datetime(df["DELIVERYDATE"], errors='coerce')
+
+idx = df.loc[:, "AREAID"]==3
+df = df.loc[idx, :]
+df = df.loc[:, ["DELIVERYDATE", "MW"]]
+
+#2024FYだけ取得
+start_date = pd.Timestamp('2023-03-31')
+end_date = pd.Timestamp('2025-04-01')
+
+idx1 = df.loc[:,"DELIVERYDATE"] > start_date
+idx2 = df.loc[:,"DELIVERYDATE"] < end_date
+idx = idx1&idx2
+df = df.loc[idx, :]
+
+#並べ直し
+df = df.sort_values(by="DELIVERYDATE")
